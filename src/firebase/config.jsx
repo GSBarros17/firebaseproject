4c799c025c0d29cb338981988831a068ada6936c
Guilherme Barros <gsbguilherme23@gmail.com>
2023-10-30 09:32:26 -0300

    add e config firebase no projeto

---
+++ src/firebase/config.jsx
@@ -0,0 +1,18 @@
+
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firebase"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAboA2xgYRCZFUhWx5mmILYE0_05c6IGP8",
+  authDomain: "zenblog-e48ca.firebaseapp.com",
+  projectId: "zenblog-e48ca",
+  storageBucket: "zenblog-e48ca.appspot.com",
+  messagingSenderId: "1019240358627",
+  appId: "1:1019240358627:web:70576bed039901e2e19bd6"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+export { db };
