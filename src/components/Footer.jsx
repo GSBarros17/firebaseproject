3bb7d34ecd4483b2de75883cf391e853c0f41847
Guilherme Barros <gsbguilherme23@gmail.com>
2023-10-30 16:41:07 -0300

    add logo ao final do footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,11 @@
 import styles from "./Footer.module.css"
+import Logo from "../img/Logo.png"
 
 export default function Footer () {
   return (
     <footer className={styles.footerContainer}>
         <h3>Escreva sobre rotinas e qualidade de vida!</h3>
-        <p>ZenBlog <span>©️</span> 2023</p>
+        <img src={Logo} alt="" /><p>ZenBlog <span>©️</span> 2023</p>
     </footer>
   )
 }
