cf173560e96db6e99048b0bcafecfd452e34de23
Guilherme Barros <gsbguilherme23@gmail.com>
2023-11-01 09:41:23 -0300

    add routas p/ as pgs createpost e dashboard

--- src/App.js
+++ src/App.js
@@ -12,6 +12,8 @@ import Login from "./pages/Login/Login"
 import Register from "./pages/Register/Register"
 import Navbar from "./components/Navbar"
 import Footer from "./components/Footer"
+import CreatePost from "./pages/CreatePost/CreatePost"
+import Dashboard from "./pages/Dashboard/Dashboard"
 //css
 import './App.css';
 
@@ -47,6 +49,8 @@ function App() {
                     <Route path="/about" element={<About/>}/>
                     <Route path="/login" element={<Login/>}/>
                     <Route path="/register" element={<Register/>}/>
+                    <Route path="/createpost" element={<CreatePost/>}/>
+                    <Route path="/dashboard" element={<Dashboard/>}/>
                   </Routes>
                 </div>
               <Footer />
