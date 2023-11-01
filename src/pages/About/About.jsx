ff80eef70889b1b3c0dbb58a7667707e4f6fedae
Guilherme Barros <gsbguilherme23@gmail.com>
2023-11-01 10:52:13 -0300

    desev da pg about

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,7 +1,13 @@
 import styles from "./About.module.css"
+import imgLogo from "../../img/Logo.png"
 
 export default function About () {
   return (
-    <div>About</div>
+    <div className={styles.about}>
+      <h1>Sobre o <span>ZEN BLOG</span></h1>
+      <p>O blog surgiu a partir de um curso de react do professor Matheus Battisti.</p>
+      <p>O projeto possui React no Front-end e Firebase no Back-end.</p>
+      <img src={imgLogo} alt="" />
+    </div>
   )
 }
