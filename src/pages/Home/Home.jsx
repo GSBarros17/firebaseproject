98b88baf906d79cd38ffdd0cf5e961397f6ffcbb
Guilherme Barros <gsbguilherme23@gmail.com>
2023-11-06 17:12:43 -0300

    dev a página Home para receber os posts

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,42 @@
 import styles from "./Home.module.css"
+import { BsSearch } from "react-icons/bs"
+import { useNavigate, Link } from "react-router-dom"
+import { useState } from "react"
+import { useAuthValue } from "../../context/AuthContext"
+
+
 
 export default function Home () {
+  
+  const {user} = useAuthValue()
+  const [query, setQuery] = useState("")
+  const [posts] = useState([])
+
+  const handleSubmit = (e) => {
+    e.preventDefalt()
+  }
+
   return (
-    <div>Home</div>
+    <div className={styles.homeContainer}>
+      <h1>Bem vindo, <span>user.displayName</span></h1>
+      <h2>Veja nossos posts mais recentes</h2>
+      <form onSubmit={handleSubmit}>
+        <input 
+          type="text" 
+          placeholder="Ou busque por tags...." 
+          onChange={(e) => setQuery(e.target.value)}  
+        />
+        <button type="button"><BsSearch/></button>
+      </form>
+      <div>
+        <h2>Posts...</h2>
+        {posts && posts.length === 0 && (
+          <div className={styles.noPosts}>
+            <p>Não foram encontrados posts</p>
+            <Link to="/createPost" className="btnForm">Criar primeiro post</Link>
+          </div>
+        )}
+      </div>
+    </div>
   )
 }
