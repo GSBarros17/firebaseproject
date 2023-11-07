af27782b32c6802fedc812b6afb24abb7a9c1181
Guilherme Barros <gsbguilherme23@gmail.com>
2023-11-07 15:56:15 -0300

    dev o body postsdetails

--- src/components/PostsDetails.jsx
+++ src/components/PostsDetails.jsx
@@ -1,7 +1,16 @@
 import styles from "./PostsDetails.module.css"
+import { Link } from "react-router-dom"
 
-export default function PostsDetails(){
+export default function PostsDetails({post}){
    return(
-    <div>posts</div>
+    <div>
+        <img src={post.image} alt={post.title}/>
+        <h2>{post.title}</h2>
+        <p>{post.createdBy}</p>
+        <div>
+            {post.tags.map((tag) => <p key={tag}><span>#</span>{tag}</p> )}
+        </div>
+        <Link to={`/posts/$post.id`} className="btnForm">ler</Link>
+    </div>
    ) 
 }
