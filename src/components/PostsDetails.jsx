bd87c0116c7305fd86066af46dbbec04dd3d5ac6
Guilherme Barros <gsbguilherme23@gmail.com>
2023-11-08 09:36:15 -0300

    alterando o css do postsDetails

--- src/components/PostsDetails.jsx
+++ src/components/PostsDetails.jsx
@@ -3,14 +3,14 @@ import { Link } from "react-router-dom"
 
 export default function PostsDetails({post}){
    return(
-    <div>
+    <div className={styles.postsCard}>
         <img src={post.image} alt={post.title}/>
         <h2>{post.title}</h2>
-        <p>{post.createdBy}</p>
-        <div>
+        <p className={styles.createdBy}>{post.createdBy}</p>
+        <div className={styles.cardTags}>
             {post.tagsArray.map((tag) => <p key={tag}><span>#</span>{tag}</p> )}
         </div>
-        <Link to={`/posts/$post.id`} className="btnForm">ler</Link>
+        <Link to={`/posts/$post.id`} className="btnForm">ler mais...</Link>
     </div>
    ) 
 }
