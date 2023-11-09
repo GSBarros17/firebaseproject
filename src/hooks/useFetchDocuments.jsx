83176b3f952e0849a16c5b9b0bf9d30fec52e97a
Guilherme Barros <gsbguilherme23@gmail.com>
2023-11-09 15:13:41 -0300

    add funcao de busca no useFetchDocuments

--- src/hooks/useFetchDocuments.jsx
+++ src/hooks/useFetchDocuments.jsx
@@ -24,8 +24,13 @@ export const useFetchDocuments = (docCollection, search = null, uid = null) => {
                 let q
                 //search
                 //dashboard
-                q = await query(collectionRef, orderBy("createAt", "desc"))
 
+                if(search){
+                    q = await query(collectionRef, where("tags", "array-contains", search), orderBy("createAt", "desc"))
+                } else {
+                    q = await query(collectionRef, orderBy("createAt", "desc"))
+                }
+                
                 await onSnapshot(q, (querySnapshot) => {
 
                     setDocuments(
