b860ab7e509093789be1a50ec61a55f32e1275e7
Guilherme Barros <gsbguilherme23@gmail.com>
2023-11-10 08:19:49 -0300

    aprimorando a funcao de busca

--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -1,15 +1,30 @@
 import styles from "./Search.module.css"
 import { useFetchDocuments } from "../../hooks/useFetchDocuments"
 import { useQuery } from "../../hooks/useQuery"
+import { Link } from "react-router-dom"
+import PostsDetails from "../../components/PostsDetails"
+
 
 export default function Search(){
     const query = useQuery()
     const search = query.get("q")
-    
+
+    const {documents: posts} = useFetchDocuments("posts", search)
+    console.log(posts)
     return (
         <div>
             <h2>Search</h2>
-            <p>{search}</p>
+            <div>
+                {posts && posts.length === 0 && (
+                    <div>
+                        <p>Não foram encontrados posts na sua perquisa.</p>
+                        <Link to="/" className="">Voltar</Link>
+                    </div>
+                )}
+                {posts && posts.map((post) => (
+                    <PostsDetails key={post.id} post={post} />
+                ))}
+            </div>
         </div>
     )
 }
