2bf9dfc2705cd5c633ddecbcaab578d33ec027b0
Guilherme Barros <gsbguilherme23@gmail.com>
2023-11-27 10:05:56 -0300

    desenvolvimento da pg welcome e seus estilos

--- src/pages/Welcome/Welcome.jsx
+++ src/pages/Welcome/Welcome.jsx
@@ -1,9 +1,21 @@
-import { styles } from "./Welcome.module.css"
+import styles from "./Welcome.module.css"
+import { Link } from "react-router-dom"
+import imgLogo from "../../img/Logo.png"
 
 export default function Welcome(){
     return(
-        <div>
-            <h1>Welcome</h1>
+        <div className={styles.welcomeContainer}>
+            <div className={styles.welcomeCard}>
+                <img src={imgLogo} alt="Logo"/>
+                <h1>Cadastro realizado com sucesso!</h1>
+                <h2>Seja bem-vindo(a) ao nosso blog.</h2>
+                <p>Compartilhe suas experiências com nossa comunidade.</p>
+                <p>Acesse nossa Home ou crie o seu primeiro post;</p>
+                <div className={styles.linksContainer}>
+                    <Link to="/" className="btnForm">Home</Link>
+                    <Link to="/createpost" className="btnForm">Criar Post</Link> 
+                </div>
+            </div>
         </div>
     )
 }
