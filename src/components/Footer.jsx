d8084cb72933fa2c61fa67d8dceab08469313676
Guilherme Barros <gsbguilherme23@gmail.com>
2024-01-08 14:20:41 -0300

    add data atual do sistema no footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,10 +2,14 @@ import styles from "./Footer.module.css"
 import Logo from "../img/Logo.png"
 
 export default function Footer () {
+  
+  let dateYear = new Date()
+  let currentYear = dateYear.getFullYear()
+  
   return (
     <footer className={styles.footerContainer}>
         <h3>Escreva sobre rotinas e qualidade de vida!</h3>
-        <img src={Logo} alt="" /><p>ZenBlog <span>©️</span> 2023</p>
+        <img src={Logo} alt="" /><p>ZenBlog <span>©️</span> {currentYear}</p>
     </footer>
   )
 }
